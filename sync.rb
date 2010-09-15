eb158641d028e2c71b8a8c04637029f720137e3e
fastred <fastred@fastred.org>
2010-09-15 17:39:55 +0200

    sync file modified

--- sync.rb
+++ sync.rb
@@ -1,14 +1,4 @@
 require 'rubygems'
-require "bundler/setup"
-require 'teambox-client'
-require 'things'
-require 'time'
-require 'timeout'
-require File.join(File.dirname(__FILE__), 'lib', 'config_store')
-require File.join(File.dirname(__FILE__), 'lib', 'task_list_cache')
-require File.join(File.dirname(__FILE__), 'lib', 'teambox_things_sync')
+require File.join(File.dirname(__FILE__), 'lib', 'teambox-things-sync')
 
-
-
-synchroniser = TeamboxThingsSync::Base.new
-synchroniser.synchronise
+TeamboxThingsSync::Base.new.synchronise
