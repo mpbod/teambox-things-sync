25514b8dccfe4e9e14c6f3a5defce669fd51d7eb
fastred <fastred@fastred.org>
2010-09-15 12:53:59 +0200

    logic moved to TeamboxThingsSync

--- sync.rb
+++ sync.rb
@@ -8,12 +8,7 @@ require File.join(File.dirname(__FILE__), 'lib', 'config_store')
 require File.join(File.dirname(__FILE__), 'lib', 'task_list_cache')
 require File.join(File.dirname(__FILE__), 'lib', 'teambox_things_sync')
 
-config = ConfigStore.new("#{ENV['HOME']}/.teambox")
-config['teambox_url'] ||= "http://teambox.com"
-config['output_info'] ||= true
-config['timeout_limit'] ||= 40
-    
-Timeout::timeout(config['timeout_limit']) {
-  synchroniser = TeamboxThingsSync.new(config)
-  synchroniser.synchronise
-}
+
+
+synchroniser = TeamboxThingsSync::Base.new
+synchroniser.synchronise
