0261b0f25d88c0931598302ce84654a45c8d9144
fastred <fastred@fastred.org>
2010-09-17 17:09:48 +0200

    added helper method returning Todo names from Today list

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,11 @@ require 'things'
 require 'time'
 require 'timeout'
 require 'rspec'
+
 Dir["#{File.dirname(__FILE__)}/../lib/*.rb"].each {|f| require f}
+
+module Things
+  def self.names_of_todos_in_today_list
+    Things::App.lists.today.reference.todos.name.get
+  end
+end
